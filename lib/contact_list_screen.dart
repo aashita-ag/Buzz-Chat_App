45afe22fab78813614aaf664c0c57ea7ecc7e717
aashita-ag <19ucs160@lnmiit.ac.in>
2021-06-22 19:34:27 +0530

    changed contact list screen

--- lib/contact_list_screen.dart
+++ lib/contact_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ContactListScreen extends StatefulWidget {
   static String id = 'contact_list_screen';
@@ -10,52 +11,85 @@ class _ContactListScreenState extends State<ContactListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFEAEAE0),
       appBar: AppBar(
-        leading: ImageIcon(
-          AssetImage('images/logo.png'),
-          size: 20,
-        ),
-        title: Text(
-          'Buzz',
-          style: TextStyle(
-            fontSize: 22,
+        backgroundColor: Color(0xFF2F5061),
+        title: Center(
+          child: Image.asset(
+            'images/appbarlogo.png',
+            height: 50,
           ),
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-
-            ),
-            Expanded(
-              child: ListView(
-                children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.account_circle,
-                        color: Colors.cyan,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Raj',
-                        ),
-                      ),
-                      Icon(
-                        Icons.phone,
-                        color: Colors.lightGreenAccent,
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.blueGrey,
-                  )
-                ],
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                //searchbar
               ),
-            ),
-          ],
+              Expanded(
+                child: ListView(
+                  children: [
+                    ContactItem(),
+                    ContactItem(),
+                    ContactItem(),
+                    ContactItem(),
+                    ContactItem(),
+                    ContactItem(),
+                    ContactItem(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ContactItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.account_circle,
+                color: Colors.cyan,
+                size: 50,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Text(
+                  'Devu',
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.phone,
+                color: Color(0xFF4297A0),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+          Divider(
+            color: Colors.blueGrey,
+            indent: 15,
+            endIndent: 15,
+          )
+        ],
       ),
     );
   }
