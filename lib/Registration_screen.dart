ae637f02853873912a64d1655bacc225a03ae58b
rajchordia14 <19ucs227@lnmiit.ac.in>
2021-06-22 16:05:08 +0530

    Add Homepage,Login,Registration screen

---
+++ lib/Registration_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'contants.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static String id = 'registration_screen';
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  late String email;
+  late String password;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'New Here?',
+                    style: TextStyle(
+                      fontSize: 45.0,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                  //Do something with the user input.
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your Password'),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: Material(
+                  elevation: 5.0,
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: MaterialButton(
+                      onPressed: null,
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
